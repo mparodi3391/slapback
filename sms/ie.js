dd9de3c36e64e8203b349235c7e4cb732dcc2bcc
Martin Parodi <mparodi3391@gmail.com>
2022-10-01 20:12:26 -0300

    acondicionamiento de div3 y embeded spotify. transiciones arrelgadas

--- sms/ie.js
+++ sms/ie.js
@@ -15,3 +15,27 @@ const elementosOcultosLeft = document.querySelectorAll('.hiddenleft');
 elementosOcultos.forEach((el)=> observador.observe(el));
 elementosOcultosRight.forEach((el)=> observador.observe(el));
 elementosOcultosLeft.forEach((el)=> observador.observe(el));
+
+const nosotrosDiv = document.querySelectorAll('.gradiente');
+
+nosotrosDiv.forEach(function(element) {
+    element.addEventListener('mouseover', (event)=>{
+        console.log(element.childNodes)
+        let textoChico = element.childNodes[3]
+        let texto = element.childNodes[1]
+        texto.style.display = 'none';
+        textoChico.style.color = 'white'
+        textoChico.style.display = 'flex'
+        textoChico.style.margin = 'auto'
+
+    })
+})
+
+nosotrosDiv.forEach(function(element) {
+    element.addEventListener('mouseout', (event)=>{
+        let textoChico = element.childNodes[3]
+        textoChico.style.display = 'none'
+        let texto = element.childNodes[1]
+        texto.style.display = 'flex';
+    })
+})
--- sms/main.js
+++ sms/main.js
@@ -3,12 +3,15 @@ window.onload = () => {
     var cabecera = document.getElementById("cabecera");
     var cambioDeColor = cabecera.offsetHeight-myNav.offsetHeight-5
 
+    document.documentElement.style.setProperty('--scroll-padding',myNav.offsetHeight-1 +'px')
+
     window.onscroll = function () {
         "use strict";
         if (document.body.scrollTop >= cambioDeColor || document.documentElement.scrollTop >= cambioDeColor) {
             myNav.classList.add("scroll");
             myNav.classList.remove("navbar");
             myNav.style.top = "0"
+            document.getElementById('nav-aparecer').style.display = 'block';
         } else {
             myNav.classList.remove("scroll");
             myNav.classList.add("navbar");
@@ -17,6 +20,7 @@ window.onload = () => {
             }
             else{
                 myNav.style.top = "0"
+                document.getElementById('nav-aparecer').style.display = 'none';
             }
             
         }
